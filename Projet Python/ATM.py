22f13bc5b23b9ada0d99f7a8e7ee224b1fc64ff5
AEKchaouche <43480300+AEKchaouche@users.noreply.github.com>
2020-03-26 01:57:08 -0400

    Update ATM.py

--- Projet Python/ATM.py
+++ Projet Python/ATM.py
@@ -4,11 +4,14 @@ Created on Thu Mar 26 00:36:16 2020
 
 @author: acer
 """
+"""
       ## Exemple instruction break
 
-Dans cet exemple, nous recherchons un nombre «88» dans la liste de nombres donnée. La condition est d'afficher tous les nombres jusqu'à ce que le nombre «88» soit trouvé et quand il est trouvé, terminer la boucle et ne pas afficher le reste des nombres.
+Dans cet exemple, nous recherchons un nombre «88» dans la liste de nombres donnée. La condition est d'afficher tous les nombres
+jusqu'à ce que le nombre «88» soit trouvé et quand il est trouvé, terminer la boucle et ne pas afficher le reste des nombres.
 
-      # programme pour afficher tous les éléments avant le numéro 88
+      ## programme pour afficher tous les éléments avant le numéro 88
+"""      
 
 for num in [11, 9, 88, 10, 90, 3, 19]:
    print(num)
